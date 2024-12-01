9f550f9f5a57e54b63a3f633e9aa0c849039abcc
ryunzz <ryan.alexander.ni@gmail.com>
2024-12-01 11:01:00 -0800

    some more formatting

--- calcraft/src/components/HomePage.js
+++ calcraft/src/components/HomePage.js
@@ -1,21 +1,12 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import { useNavigate } from 'react-router-dom';
-import LogoCalCraft from './LogoCalCraft.png';
-// import logo from '/.logo.png';
+
+
+
+
+
+
+
+
 
 import me from '../assets/me.png'
 import siera from '../assets/siera.png'
@@ -26,35 +17,105 @@ const HomePage = () => {
   return (
     <div class="pageContent2">
       <div class="pageContent">
-        {/* <Typography
-          variant="h4"
-          noWrap
-          component="a"
-          href="/home"
-          sx={{
-            mr: 2,
-            display: { xs: 'none', md: 'flex' },
-            fontFamily: 'monospace',
-            fontWeight: 700,
-            letterSpacing: '.3rem',
-            color: 'inherit',
-            textDecoration: 'none',
-            fontSize: '50px',
-          }} 
-        >  
-          Insulin Injestion Quantity Calculator</Typography> */}
-        {/*Authored by Sierra J.*/}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         <h3> Cal Craft </h3>
         <div class="info"> 
           <p>Opt either to take a photo of the food you will eat, or select from our friendly menu, and we'll tell you how many units of fast-acting insulin to injest!</p>
         </div>
         <h2> Our team </h2>
         <div class="team">
+          <div>
+            <a href="https://www.linkedin.com/in/ryan-ni/" target="_blank" rel="noreferrer">
+              <img src={me} alt="Ryan Ni"></img>
+            </a>Ryan Ni
+            <br>
+            </br>
+            ML • Backend • DB
+          </div>
+          <div>
+            <a href="https://www.linkedin.com/in/sankritya-rai-002a40261/" target="_blank" rel="noreferrer">
+              <img src={sankritya} alt="Sankritya Rai"></img>
+            </a>
+            Sankritya Rai
+            <br></br>
+            ML • Backend
+          </div>
+          <div>
+            <a href="https://www.linkedin.com/in/ryleebao/" target="_blank" rel="noreferrer">
+              <img src={rylee} alt="Rylee Bao"></img>
+            </a>
+            Rylee Bao
+            <br>
+            </br>
+            Frontend • Backend
+          </div>
+          <div>
+            <a href="https://www.linkedin.com/in/sierra-j-06175b216/" target="_blank" rel="noreferrer">
+              <img src={siera} alt="Sierra Janson"></img>
+            </a>
+            Sierra Janson
+            <br>
+            </br>
+            Frontend
+          </div>
           
+        
         </div>
       </div>
     </div>
   );
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default HomePage;
